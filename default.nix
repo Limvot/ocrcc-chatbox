2d1ca4927c3727cdc2056549870809a8eb8a83c1
Nathan Braswell <miloignis@gmail.com>
2020-04-29 22:42:21 -0400

    live debugging, oh my

--- default.nix
+++ default.nix
@@ -1,12 +1,9 @@
 with (import <nixpkgs> {});
-#with (import /home/maarten/code/nixos/yarn2nix { inherit pkgs; });
-rec {
-  weave-front-end = mkYarnPackage {
-    name = "safesupport-chatbox";
-    src = ./.;
-    packageJSON = ./package.json;
-    yarnLock = ./yarn.lock;
-    # NOTE: this is optional and generated dynamically if omitted
-    yarnNix = ./yarn.nix;
-  };
+mkYarnPackage {
+  name = "safesupport-chatbox";
+  src = ./.;
+  packageJSON = ./package.json;
+  yarnLock = ./yarn.lock;
+  # NOTE: this is optional and generated dynamically if omitted
+  yarnNix = ./yarn.nix;
 }
